07cfa5ef18b0edf42e168aca064c68d1c732e484
Vitaly Shvedko <davids013013@gmail.com>
2022-11-20 14:23:06 +0300

    JsonProperty for AuthToken's field was added

--- src/main/java/ru/netology/diploma_cloud_storage/domain/AuthToken.java
+++ src/main/java/ru/netology/diploma_cloud_storage/domain/AuthToken.java
@@ -1,5 +1,6 @@
 package ru.netology.diploma_cloud_storage.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,5 +16,6 @@ import javax.validation.constraints.Pattern;
 public class AuthToken {
     @Length(min = 1, max = 256)
     @Pattern(regexp = "[\\w\\d\\s-.]+")
+    @JsonProperty("auth-token")
     private String authToken;
 }
